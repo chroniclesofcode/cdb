f66d617b4c723925121552210397b713b6b187c8
Donald Tang <donald.tang.email@googlemail.com>
2024-03-20 13:58:15 +1100

    Added constructors in cpp

--- include/cdb/Table.hpp
+++ include/cdb/Table.hpp
@@ -10,9 +10,9 @@ class Table
 {
 public:
 
-    Table() : Table({"sym", "price", "size"}, {"string", "int", "int"}) {}
+    Table();
 
-    Table(std::vector<std::string> _columns, std::vector<std::string> _dtypes) : columns{std::move(_columns)}, dtypes{std::move(_dtypes)} {}
+    Table(std::vector<std::string> _columns, std::vector<std::string> _dtypes);
     
     void addEntry(std::string key, int32_t value);
 
--- src/cdb/Table.cpp
+++ src/cdb/Table.cpp
@@ -2,6 +2,10 @@
 
 namespace cdb
 {
+    Table::Table() : Table::Table({"sym", "price", "size"}, {"string", "int", "int"}) {}
+
+    Table::Table(std::vector<std::string> _columns, std::vector<std::string> _dtypes) : columns{std::move(_columns)}, dtypes{std::move(_dtypes)} {}
+
     void Table::addEntry(std::string key, int32_t value)
     {
         data[key] = value;
